28dbdbaef2565ab15672889371d51cbb6e427f13
lukasz.danelczyk <lukasz.danelczyk.sp@lhsystems.com>
2022-05-25 18:24:48 +0200

    Update spring cloud version and clean up

--- src/main/java/com/lddelz/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/com/lddelz/springbootadmin/SpringBootAdminApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
 
 }
